fd4b9e58c6ae8403b7260a45967c3d708f42108f
smooto <melissasmoot@live.com>
2020-04-06 11:33:16 -0700

    init alchemy bootstrap

---
+++ fruits.js
@@ -0,0 +1,73 @@
+const apple = {
+    id: 'apple',
+    name: 'Red Apple',
+    image: 'apple.png',
+    description: 'A sweet, delicious, forbidden-to-some treat',
+    category: 'tree-fruit',
+    price: 1.00,
+    cost: 0.25
+};
+const banana = {
+    id: 'banana',
+    name: 'Banana',
+    image: 'banana.png',
+    description: 'Yellow and slippery',
+    category: 'tropical',
+    price: 1.04,
+    cost: 0.54
+};
+const blueberry = {
+    id: 'blueberry',
+    name: 'Blueberry',
+    image: 'blueberry.png',
+    description: 'Little round blue gumballs',
+    category: 'berry',
+    price: 4.00,
+    cost: 2.00
+};
+const cherry = {
+    id: 'cherry',
+    name: 'Cherry',
+    image: 'cherry.png',
+    description: 'July favorite',
+    category: 'tree-fruit',
+    price: 4.00,
+    cost: 1.75
+};
+const greenApple = {
+    id: 'green-apple',
+    name: 'Green Apple',
+    image: 'green-apple.jpg',
+    description: 'What granny made pies from',
+    category: 'tree-fruit',
+    price: 1.34,
+    cost: .34
+};
+const mango = {
+    id: 'mango',
+    name: 'Mango',
+    image: 'mango.png',
+    description: 'Rhymes with tango',
+    category: 'In everything these days',
+    price: 7.50,
+    cost: .12
+};
+const orange = {
+    id: 'orange',
+    name: 'Orange',
+    image: 'orange.png',
+    description: 'Please peel before eating',
+    category: 'tree-fruit',
+    price: .76,
+    cost: .75
+};
+const fruits = [
+    apple,
+    banana,
+    blueberry,
+    cherry,
+    greenApple,
+    mango,
+    orange
+];
+export default fruits;
