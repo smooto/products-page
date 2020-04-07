434722520482e689d80c4f2f54fadab412f2baae
smooto <melissasmoot@live.com>
2020-04-06 17:15:32 -0700

    add basic styling

---
+++ common/app.js
@@ -0,0 +1,16 @@
+// import product data
+import products from '/products/products.js';
+import createProduct from '/products/createProduct.js';
+
+// get container from DOM
+const container = document.getElementById('products-container');
+
+// render products on page
+function renderProducts(arrayOfProducts) {
+    arrayOfProducts.forEach(product => {
+        const li = createProduct(product);
+        container.appendChild(li);
+    });
+}
+
+renderProducts(products);
