46a2a899874f189122bc95e946c8fae18b7f9d71
smooto <melissasmoot@live.com>
2020-04-09 17:50:29 -0700

    add form functionality to admin page

--- product-entry/product-entry-app.js
+++ product-entry/product-entry-app.js
@@ -1,10 +1,27 @@
+import addProduct from './add-product.js';
+
 // get form element and submit button from DOM
 const newProductForm = document.getElementById('new-product-form');
-const submitButton = document.getElementById('submit-button');
 
-submitButton.addEventListener('submit', (e) => {
+newProductForm.addEventListener('submit', (e) => {
+    // prevent button from reloading page
     e.preventDefault();
-    const newProductData = new FormData(newProductForm);
 
-    
+    // get form inputs, store as new object
+    const productData = new FormData(newProductForm);
+
+    const newProductObject = {
+        id: productData.get('id'),
+        name: productData.get('name'),
+        image: productData.get('image'),
+        description: productData.get('description'),
+        category: productData.get('category'),
+        price: productData.get('price'),
+        cost: productData.get('cost')
+    };
+
+    console.log(newProductObject);
+
+    // add new object to product list
+    addProduct(newProductObject);
 });
